0c5e10d2cdfa65f5a9f49ae50bd6462d0b56bf84
nateyoungren <nathanaelyoungren@gmail.com>
2019-03-06 13:01:51 -0800

    Setup horizontal tests.

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -11,8 +11,14 @@ import XCTest
 
 class GameAITests: XCTestCase {
     
+    var board: GameBoard!
+    
+    override func setUp() {
+        super.setUp()
+        board = GameBoard()
+    }
+    
     func testWinCheckingVertical1() {
-        var board = GameBoard()
         /*
          x o -
          x o -
@@ -28,7 +34,6 @@ class GameAITests: XCTestCase {
     }
     
     func testWinCheckingVertical2() {
-        var board = GameBoard()
         /*
          x o -
          x o -
@@ -44,25 +49,36 @@ class GameAITests: XCTestCase {
     }
     
     func testWinCheckingHorizontal1() {
-        var board = GameBoard()
         /*
          - o -
          x x x
          o - -
          */
+        try! board.place(mark: .x, on: (0, 1))
+        try! board.place(mark: .o, on: (1, 0))
+        try! board.place(mark: .x, on: (1, 1))
+        try! board.place(mark: .o, on: (0, 2))
+        try! board.place(mark: .x, on: (2, 1))
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
     }
     
     func testWinCheckingHorizontal2() {
-        var board = GameBoard()
         /*
          x - -
          - x -
          o o o
          */
+        try! board.place(mark: .o, on: (0, 2))
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .o, on: (1, 2))
+        try! board.place(mark: .x, on: (1, 1))
+        try! board.place(mark: .o, on: (2, 2))
+        XCTAssertTrue(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
     func testWinCheckingDiagonal1() {
-        var board = GameBoard()
         /*
          x - -
          - x -
@@ -71,7 +87,6 @@ class GameAITests: XCTestCase {
     }
     
     func testWinCheckingDiagonal2() {
-        var board = GameBoard()
         /*
          x - o
          - o -
